003f0429686531731dd6ddfb702df4184fede0cc
dmswl1779 <57386045+dmswl1779@users.noreply.github.com>
2019-12-19 14:39:12 +0900

    Add files via upload

---
+++ js/jquery.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){ 
+$(".mainSliderSlick").slick({
+	dots: true,
+	infinite: true,
+	arrows: false,
+	slidesToShow: 1,
+	slidesToScroll: 1,
+	autoplay:true,
+	autoplaySpeed:3000,
+	fade: true,
+	fadeSpeed:1000,
+	pauseOnHover:true,
+  });
+})
